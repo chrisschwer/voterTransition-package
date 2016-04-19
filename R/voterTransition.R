ac5ea73ea98b1b8a4f7b910bd5c630d3eaa31d1b
chrisschwer <christophschwerdtfeger@christophs-mbp.speedport_w_723v_1_39_000>
2016-04-19 22:35:43 +0200

    removed formulas that do not translate well into help files

--- R/voterTransition.R
+++ R/voterTransition.R
@@ -4,12 +4,13 @@
 #' 
 #' Formally: Given a previous set of voting choices in matrix \eqn{O} of size \eqn{m x n} and a new set of
 #' voting choices in matrix \eqn{N} of size \eqn{m x p}, a transition matrix \eqn{T} of size \eqn{p x n} is 
-#' optimal if it minimizes the error defined as 
-#' $$\sum_{i=1}^p \sum_{j=1}^n\left( N_{i,j}-M_{i,j} \right)^2$$
-#' where \eqn{M = O T}, while for all \eqn{i, j} \eqn{T_{i,j} >= 0} and for all \eqn{i} \eqn{\sum_{j=1}^p T_{i,j} = 1}. 
+#' optimal if it minimizes the error defined as the squared difference between the predicted
+#' outcome in the second election (\eqn{O T}) and the actual outcome in the new election
+#' (\eqn{N}). All elements of \eqn{T} are non-negative and all row sums of \eqn{T} are 1.
 #' 
 #' Core function is \code{\link{optimTransition}}, with a variant 
 #' \code{\link{optimTransitionCluster}} supporting the split of the matrices by clusters.
 #' 
+#' @docType package
 #' @name voterTransition
-"_PACKAGE"
+NULL
